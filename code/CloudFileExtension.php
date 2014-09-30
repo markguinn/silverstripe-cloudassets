1afc348ce1038bbaa341137760754777bf446f13
Mark Guinn <mark@adaircreative.com>
2014-09-30 12:37:45 +0100

    Spaces => Tabs in CloudFileExtension.php

--- code/CloudFileExtension.php
+++ code/CloudFileExtension.php
@@ -262,8 +262,8 @@ class CloudFileExtension extends DataExtension
 	 */
 	public function createLocalIfNeeded() {
 		if ($this->owner->CloudStatus === 'Live') {
-            $bucket = $this->getCloudBucket();
-            if ($bucket && $bucket->isLocalCopyEnabled()) {
+			$bucket = $this->getCloudBucket();
+			if ($bucket && $bucket->isLocalCopyEnabled()) {
 				if (!file_exists($this->owner->getFullPath()) || $this->containsPlaceholder()) {
 					try {
 						$this->downloadFromCloud();
