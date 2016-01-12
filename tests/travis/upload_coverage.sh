c132efe2583b29c7e33adccaa1229723dfe0b790
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-12 11:53:19 +0700

    FIX: Remove now obsolete upload coverage script

--- tests/travis/upload_coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ -n "$COVERAGE" ]; then
-	cd cloudassets
-	wget https://scrutinizer-ci.com/ocular.phar
-	php ocular.phar code-coverage:upload -v --format=php-clover coverage.xml
-fi
